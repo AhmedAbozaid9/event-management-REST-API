aebe53b46aa0a94b119bb53508424d646d60a483
Ahmed <ahmedabozaid.dev@gmail.com>
2024-11-27 16:56:14 +0200

    adds db relations and routing

---
+++ app/Models/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function attendees()
+    {
+        return $this->hasMany(Attendee::class);
+    }
+}
